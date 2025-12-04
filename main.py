d7037d8fc9f1f94f126569495a63bad604838d6d
LarryG0221 <76797293+LarryG0221@users.noreply.github.com>
2025-12-04 08:12:39 -0500

    modify some actions
    ship dose not need to fire bullet.
    Aliens do.

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import sys
 import pygame
 from pygame.sprite import Group
 import game_functions as gf
-from settings import Settings
+from game_functions import Settings
 from game_functions import Ship
-from game_stats import GameStats
-from button import Button
+from game_functions import GameStats
+from game_functions import Button
 from scoreboard import Scoreboard
-from pygame.sprite import Sprite
+import random
 
 
 def run_game():
@@ -17,57 +17,35 @@ def run_game():
 
     ai_setting = Settings()
     screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
-    clock = pygame.time.Clock()
-    font = pygame.font.Font(None, 40)  # default font, size 40
 
-    start_time = pygame.time.get_ticks()
     background = pygame.image.load("images/bg.png")
+    background = pygame.transform.smoothscale(background, (ai_setting.screen_width, ai_setting.screen_height))
 
     ship = Ship(screen, ai_setting)
-    bullets = Group()
+
     aliens = Group()
+    alien_bullets = Group()
     stats = GameStats(ai_setting)
     play_button = Button(ai_setting, screen, 'Play')
     sb = Scoreboard(ai_setting, screen, stats)
 
     gf.create_fleet(ai_setting, screen, aliens, ship)
-    # Get elapsed time in seconds
-    elapsed_ms = pygame.time.get_ticks() - start_time
-    elapsed_sec = elapsed_ms // 1000
-
-    # Format as mm:ss
-    minutes = elapsed_sec // 60
-    seconds = elapsed_sec % 60
-    time_text = f"{minutes:02}:{seconds:02}"
-
-    # Render
-    scoreboard = font.render(f"Time: {time_text}", True, (255, 255, 255))
-
     screen.fill((0, 0, 0))
 
     while True:
         screen.blit(background, (0, 0))
-        screen.blit(scoreboard, (120, 20))
-        # pygame.display.flip()
 
-        gf.check_events(ai_setting, screen, ship, bullets, aliens, stats, play_button, sb)
+        gf.check_events(ai_setting, screen, ship, alien_bullets, aliens, stats, play_button, sb)
         if stats.game_active:
             ship.update()
-            gf.update_bullets(bullets, ai_setting, screen, aliens, ship, stats, sb)
-            gf.update_aliens(ai_setting, stats, screen, aliens, ship, bullets, sb)
-        gf.update_screen(ai_setting, screen, ship, aliens, bullets, stats, play_button, sb)
+            gf.update_aliens(ai_setting, stats, screen, aliens, ship, sb, alien_bullets)
+        gf.update_screen(screen, ship, aliens, alien_bullets, stats, play_button, sb )
         if  not circulation:
             break
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                print(' score ', stats.high_score)
-                with open('score.txt', 'w') as f:
-                    f.write(str(stats.high_score))
-                sys.exit()
-
-            # Update display
-
+        # save score in a text file
+        with open('score.txt', 'w') as f:
+            f.write(str(stats.score))
         pygame.display.update()
 
 run_game()
